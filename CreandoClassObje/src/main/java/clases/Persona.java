5cbbc5cde03968ced9a853c5a7a8a7c0012a6097
unknown <ervinjoel2323@gmail.com>
2021-03-16 10:49:46 -0600

    Creacion de clase persona y agregando atributos

---
+++ CreandoClassObje/src/main/java/clases/Persona.java
@@ -0,0 +1,17 @@
+
+package clases;
+
+public class Persona {
+    //atributos de la clase
+    String nombre;
+    String apellido;
+    
+    public void desplegarInformacion(){
+        System.out.println("Nombre:" + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
