66eaa475b3d0f4442666a858c340696175b11bed
unknown <ervinjoel2323@gmail.com>
2021-03-16 10:09:14 -0600

    terminando lo basico, agregando ejercicios y comenzando al mundo de las clases

--- Sintaxis_JavaEjemplos/src/main/java/operaCondici.java
+++ Sintaxis_JavaEjemplos/src/main/java/operaCondici.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class operaCondici{
@@ -10,8 +9,11 @@ public class operaCondici{
         boolean resultado = a >= valorMinimo && a <= valorMaximo;
         System.out.println("resultado = " + resultado);
         
+        // Iniciamos la variable vacaciones false y diaDescanso como true
         boolean vacaciones = false;
         boolean diaDescanso = true;
+        //cuando entra en el if las variables estan definidas como true y false, esto le llamamos "bandera"
+        //usamos el operaodr de condicion or para revisar si cualqueira de los dos es true, devuelve un true, solo devolvera un false si los dos son false
         if(vacaciones || diaDescanso)
             System.out.println("Padre puede asistir al juego del hijo");
         else
