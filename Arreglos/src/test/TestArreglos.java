b0ff7513a2f77b8233a325d2bb202c3fe70b2165
Ervin <ervinjoel2323@gmail.com>
2021-03-26 14:22:52 -0400

    Agregando comentarios en clase arreglos

--- Arreglos/src/test/TestArreglos.java
+++ Arreglos/src/test/TestArreglos.java
@@ -4,10 +4,12 @@ public class TestArreglos {
 
     public static void main(String[] args) {
         int edades[] = new int[3];
+        /*solo imprimimos la variable arreglo, esto devolvera solo la ubicacion en la memoria
+        ya que no tiene valores dentro de sus espacios*/
         System.out.println("edades" + edades);
-
+        // ingresamos un valor en la ubicacion 0 del arreglo, la ubicacion empieza desde 0 hasta n-1
         edades[0] = 10;
-
+        //Ahora imprimimos lo que esta en la posicion del arreglo numero 0
         System.out.println("elemento 0: " + edades[0]);
     }
 }
