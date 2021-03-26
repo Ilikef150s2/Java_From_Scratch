1c60b88873a4db61c2416876ac6650b2960b9f78
ervin <ervinjoel2323@gmail.com>
2021-03-25 19:56:32 -0600

    Usando sobrecarga de metodos, ejemplo con dos metodos con el mismo nombre

---
+++ SobreCargaMetodos/src/operaciones/Operaciones.java
@@ -0,0 +1,14 @@
+
+package operaciones;
+
+public class Operaciones {
+    public static int sumar(int a, int b){
+        System.out.println("sumando int a + b");
+        return a + b;
+    }
+    
+    public static double sumar(double a, double b){
+        System.out.println("sumando double a + b");
+        return a + b;
+    }
+}
