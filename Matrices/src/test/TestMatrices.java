90c45f8a76aafb11aa0a68ee809286883e91551f
Ervin <ervinjoel2323@gmail.com>
2021-03-29 10:27:17 -0400

    ingresando valores en matriz 3x2

--- Matrices/src/test/TestMatrices.java
+++ Matrices/src/test/TestMatrices.java
@@ -8,5 +8,15 @@ public class TestMatrices {
         int edades[][] = new int [3][2];
         System.out.println("edades: " + edades);
         //si imprimimos esta variable nos devolvera la direccion de memoria
+        
+        edades[0][0] = 1;
+        edades[0][1] = 2;
+        edades[1][0] = 3;
+        edades[1][1] = 4;
+        /*podemos ingresar datos en la matriz de esta forma
+        diciendo primero las fila sy luego las columnas*/
+        System.out.println("edades 0-0: " + edades[0][0]);
+        System.out.println("edades 0-1: " + edades[0][1]);
+        System.out.println("edades 1-0: " + edades[1][0]);
     }
 }
