ac88d6fa056843511b929ab6a41ef280cb559eac
Ervin <ervinjoel2323@gmail.com>
2021-03-29 10:42:09 -0400

    iterando matriz e imprimiendola con ciclo for anidado

--- Matrices/src/test/TestMatrices.java
+++ Matrices/src/test/TestMatrices.java
@@ -1,14 +1,14 @@
-
 package test;
 
 public class TestMatrices {
+
     public static void main(String[] args) {
         /*creamos el arreglo de enteros y luego lo instanciamos diciendo que es entero
         y que tiene 3 filas y 2 columnas, recordar que se cuenta desde 0*/
-        int edades[][] = new int [3][2];
+        int edades[][] = new int[3][2];
         System.out.println("edades: " + edades);
         //si imprimimos esta variable nos devolvera la direccion de memoria
-        
+
         edades[0][0] = 1;
         edades[0][1] = 2;
         edades[1][0] = 3;
@@ -18,5 +18,16 @@ public class TestMatrices {
         System.out.println("edades 0-0: " + edades[0][0]);
         System.out.println("edades 0-1: " + edades[0][1]);
         System.out.println("edades 1-0: " + edades[1][0]);
+
+        //usaremos un for para iterar la matriz
+        //ren significa reglones
+        //col significa columnas
+        System.out.println("Iterando con ciclo for \n");
+        for (int ren = 0; ren < edades.length; ren++) {
+            //ponemos edades[ren].length para devolver el largo del reglon "2"
+            for (int col = 0; col < edades[ren].length; col++) {
+                System.out.println("edades: " + ren + "-" + col + ": " + edades[ren][col]);
+            }
+        }
     }
 }
