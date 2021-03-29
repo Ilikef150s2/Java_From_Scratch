38ad1490bc8a320b5ad2bdb87fd99d5aadc89182
Ervin <ervinjoel2323@gmail.com>
2021-03-29 12:23:35 -0400

    creando metodo imprimir para la matriz de objeto de tipo persona

--- Matrices/src/test/TestMatrices.java
+++ Matrices/src/test/TestMatrices.java
@@ -1,5 +1,7 @@
 package test;
 
+import domain.Persona;
+
 public class TestMatrices {
 
     public static void main(String[] args) {
@@ -38,6 +40,34 @@ public class TestMatrices {
             for (int col = 0; col < frutas[ren].length; col++) {
                 //aplicamos lo mismo que al principio le damos al for interno que recorra el tama;o del reglon que es 2
                 System.out.println("Fruta en posicion: " + ren + "-" + col + ": " + frutas[ren][col]);
+                //mandamos a imprimir los valores que encuentra en la matriz.
+            }
+
+        }
+
+        System.out.println("Impresion de matriz de objetos");
+        //cremos la matriz de tipo objeto persona y le decimos que cree una nueva que tenga 2x3
+        Persona personas[][] = new Persona[2][3];
+        imprimir(frutas); //aqui solo llamamos el metodo que creamos de imprimir 
+        System.out.println("Separando matrices");
+        personas[0][0] = new Persona("Joel");
+        personas[1][0] = new Persona("Ervin");
+        personas[1][1] = new Persona("Moreno");
+        imprimir(personas);
+    }
+
+    /*Crearemos un metodo par ano repetir el codigo de imprimir
+    con el for iterando en cada espacio de la matriz*/
+
+ /*Se crea el metodo de imprimir matriz y dentro de los argumentos
+    se pone el tipo que es object y luego los arumentos
+    en este caso e suna matriz, no es necesario agregarlo valor
+    por que solo es paso de argumentos*/
+    public static void imprimir(Object matriz[][]) {
+        for (int ren = 0; ren < matriz.length; ren++) {
+            for (int col = 0; col < matriz[ren].length; col++) {
+                //aplicamos lo mismo que al principio le damos al for interno que recorra el tama;o del reglon
+                System.out.println("Matriz en posicion: " + ren + "-" + col + ": " + matriz[ren][col]);
                 //mandamos a imprimir los valores que encuentra en la matriz.
             }
 
