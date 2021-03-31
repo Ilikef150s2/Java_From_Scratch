09f38a57c2a233a8b9206a49abd00c0fbf93708b
Ervin <ervinjoel2323@gmail.com>
2021-03-31 15:42:59 -0400

    Creando proyecto para detectar un texto si es palidromo o no

---
+++ Palindromo/src/Palindromo.java
@@ -0,0 +1,15 @@
+
+public class Palindromo {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static boolean detecPalindromo(String texto){/*Le pasamos un String texto ya 
+        que asi podra leer, numeros o cadenas de caracteres*/
+        
+        /*Para leer mejro la cadena y verificar si es palidromo lo mejor 
+        seria reducir todo a minusculas, por eso usaremos la funcion tolowercase*/
+
+        texto = texto.toLowerCase();
+    }
+}
