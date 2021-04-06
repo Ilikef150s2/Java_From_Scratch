ef31a9f7130a0eae9ddb1c7e9d5bf59bd93c5fd2
ervin <ervinjoel2323@gmail.com>
2021-04-05 18:43:35 -0600

    actualizando comentarios /97

--- Ventas/src/ni/com/mg/ventas/test/VentasTest.java
+++ Ventas/src/ni/com/mg/ventas/test/VentasTest.java
@@ -9,6 +9,8 @@ public class VentasTest {
         Producto producto2 = new Producto("Pantalon", 100.0);//creamos el primer producto
         Producto producto3 = new Producto("Cel", 200.0);//creamos el primer producto
         Orden orden1 = new Orden();
+        //llamamos los metodos con los datos que le agregamos
+        /*orden1 es el objeto para mandar a llamar al metodo o clase agregarProducto*/
         orden1.agregarProducto(producto1);
         orden1.agregarProducto(producto2);
         orden1.agregarProducto(producto3);
