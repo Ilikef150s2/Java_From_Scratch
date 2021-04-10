25ed33cc45ebb448922ebbbcbf43f2e5fa6e778c
ervin <ervinjoel2323@gmail.com>
2021-04-10 12:41:39 -0600

    Conociendo forEach

---
+++ ForEach/src/test/TestForEach.java
@@ -0,0 +1,11 @@
+
+package test;
+
+public class TestForEach {
+    public static void main(String[] args) {
+        int edades[] = {1,3,5,3};
+        for(int edad: edades){
+            System.out.println("edad = " + edad);
+        }
+    }
+}
