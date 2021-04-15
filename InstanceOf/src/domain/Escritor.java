740366ce262d4ef9d143502ede533f6b782f8fb0
ervin <ervinjoel2323@gmail.com>
2021-04-14 21:24:48 -0600

    Conversion de objetos

---
+++ InstanceOf/src/domain/Escritor.java
@@ -0,0 +1,16 @@
+
+package domain;
+
+public class Escritor extends Empleado {
+   final TipoEscritura tipoEscritura;
+   
+   public Escritor(String nombre, double sueldo, TipoEscritura tipoEscritura){
+       super(nombre, sueldo);
+       this.tipoEscritura = tipoEscritura;
+   }
+   
+   @Override
+   public String obtenerDetalles(){
+       return super.obtenerDetalles() + ". tipoEscritura" + tipoEscritura.getDescripcion();
+   }
+}
