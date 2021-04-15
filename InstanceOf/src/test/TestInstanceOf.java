28593275fd99a464554304322c6c3510d20362db
ervin <ervinjoel2323@gmail.com>
2021-04-14 20:45:41 -0600

    Usando instanceof para verificar tipo de datos

--- InstanceOf/src/test/TestInstanceOf.java
+++ InstanceOf/src/test/TestInstanceOf.java
@@ -3,13 +3,25 @@ package test;
 import domain.*;
 
 public class TestInstanceOf {
-    public static void main(String[] args) {
-       Empleado empleado = new Empleado("Juan", 5000);
 
-       empleado = new Gerente("Karla", 10000, "Contabilidad");
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado("Juan", 5000);
+        //determinarTipo(empleado);
+        empleado = new Gerente("Karla", 10000, "Contabilidad");
+        determinarTipo(empleado);
     }
-    
-    public static void determinarTipo(Empleado empleado){
-        
+
+    public static void determinarTipo(Empleado empleado) {
+        if (empleado instanceof Gerente) {
+            /*Le preguntamos si la variable empleado esta 
+            apuntando a un objeto llamado Gerente en memoria*/
+            System.out.println("Es de tipo Gerente");
+        }
+        else if (empleado instanceof Empleado) {
+            System.out.println("Es de tipo Empleado");
+        }
+        else if (empleado instanceof Object) {
+            System.out.println("Es de tipo Object");
+        }
     }
 }
