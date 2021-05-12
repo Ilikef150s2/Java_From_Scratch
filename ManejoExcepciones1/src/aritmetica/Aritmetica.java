69a0714c53df830d0925bf894dd0ce850b828b4d
ervin <ervinjoel2323@gmail.com>
2021-05-12 14:40:58 -0600

    Manejo de excepciones clase exception parte2

--- ManejoExcepciones1/src/aritmetica/Aritmetica.java
+++ ManejoExcepciones1/src/aritmetica/Aritmetica.java
@@ -4,7 +4,7 @@ import excepciones.OperacionExcepcion;
 
 public class Aritmetica {
 
-    public static int division(int numerador, int denominador) throws OperacionExcepcion {
+    public static int division(int numerador, int denominador){
         if (denominador == 0) {
             throw new OperacionExcepcion("Division entre cero");
         }
