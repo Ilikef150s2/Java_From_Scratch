40be5b1b408e55eb21cbd23b73b406796a9d9b9c
ervin <ervinjoel2323@gmail.com>
2021-05-12 11:55:22 -0600

    Manejo de excepciones

---
+++ ManejoExcepciones1/src/test/TestExcepciones.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestExcepciones {
+    public static void main(String[] args) {
+        int resultado = 0;
+        try{
+            resultado = 10/0;
+        }catch(Exception e){
+            e.printStackTrace(System.out);
+        }
+        System.out.println("Resultado = " + resultado);
+    }
+}
